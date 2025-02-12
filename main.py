f308a03453e4a63b75644929ec7fe2ccbf89e682
amagklara <anastasiamaglara12@gmail.com>
2025-02-12 13:00:06 +0200

    load file

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import pandas as pd
+from ucimlrepo import fetch_ucirepo
 
-churn = pd.read_csv('dataset/customer_churn.csv')
-print(churn)
+# fetch dataset
+iranian_churn = fetch_ucirepo(id=563)
+
+# data (as pandas dataframes)
+X = iranian_churn.data.features
+y = iranian_churn.data.targets
+
+# metadata
+print(iranian_churn.metadata)
+
+# variable information
+print(iranian_churn.variables)
